f70060e41acf4196335635eaa109965dd8765118
samuelololol <samuelololol@gmail.com>
2016-07-14 00:01:58 +0800

    update

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM nginx
+MAINTAINER samuelololol <samuelololol@gmail.com>
+EXPOSE 4281
+RUN apt-get update;\
+    apt-get install ssh wget vim;\
+    apt-get clean;\
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 echo
-if [ ! -f apt_updated ]; then
+cd /
+if [ ! -f provision ]; then
     echo "Do apt-get update.."
     echo
-    touch apt_updated
+    touch provision
+    echo "Do apt-get install .."
     apt-get update
+    apt-get -y install ssh wget 
+    echo "apt-get install done"
 fi
-echo "Do apt-get install .."
-echo
-apt-get -y install ssh wget 
 cp /app/nginx.conf /etc/nginx/
+echo "[docker]prepare to tunnel"
+echo "[docker]ssh -nNT -L "$INNER_PORT:$TARGET_INNER_IP:$TARGET_INNER_PORT $TARGET_ACC@$TARGET_IP
 ssh -nNT -L $INNER_PORT:$TARGET_INNER_IP:$TARGET_INNER_PORT $TARGET_ACC@$TARGET_IP &
+echo "[docker]tunnel"
+sed -i "s/EXPOSE_PORT/${EXPOSE_PORT}/" /etc/nginx/nginx.conf
+sed -i "s/INNER_PORT/${INNER_PORT}/" /etc/nginx/nginx.conf
+echo "[docker]start nginx ..."
 nginx -g "daemon off;"
 
