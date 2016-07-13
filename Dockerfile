d2978998955ba333e295b681fb9d23755918081f
samuelololol <samuelololol@gmail.com>
2016-07-14 00:33:50 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM nginx
 MAINTAINER samuelololol <samuelololol@gmail.com>
 EXPOSE 4281
 RUN apt-get update;\
-    apt-get install ssh wget vim;\
+    apt-get -y install ssh wget vim;\
     apt-get clean;\
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
--- install.sh
+++ install.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-echo
-cd /
-if [ ! -f provision ]; then
-    echo "Do apt-get update.."
-    echo
-    touch provision
-    echo "Do apt-get install .."
-    apt-get update
-    apt-get -y install ssh wget 
-    echo "apt-get install done"
-fi
 cp /app/nginx.conf /etc/nginx/
 echo "[docker]prepare to tunnel"
-echo "[docker]ssh -nNT -L "$INNER_PORT:$TARGET_INNER_IP:$TARGET_INNER_PORT $TARGET_ACC@$TARGET_IP
-ssh -nNT -L $INNER_PORT:$TARGET_INNER_IP:$TARGET_INNER_PORT $TARGET_ACC@$TARGET_IP &
+echo "[docker]ssh -oStrictHostKeyChecking=no -i /ssh/id_rsa -nNT -L "$INNER_PORT:$TARGET_INNER_IP:$TARGET_INNER_PORT $TARGET_ACC@$TARGET_IP
+ssh -oStrictHostKeyChecking=no -i /ssh/id_rsa -nNT -L $INNER_PORT:$TARGET_INNER_IP:$TARGET_INNER_PORT $TARGET_ACC@$TARGET_IP &
 echo "[docker]tunnel"
 sed -i "s/EXPOSE_PORT/${EXPOSE_PORT}/" /etc/nginx/nginx.conf
 sed -i "s/INNER_PORT/${INNER_PORT}/" /etc/nginx/nginx.conf
